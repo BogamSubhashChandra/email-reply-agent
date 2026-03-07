604dfdc47b04531868602c377be5439b7137b0aa
2503b05203 <2503b05203@sru.edu.in>
2026-03-07 22:11:21 +0530

    fix

--- agent.py
+++ agent.py
@@ -1,19 +1,29 @@
 import os
-import google.generativeai as genai
+import requests
 
-# configure Gemini API
-genai.configure(api_key=os.environ.get("GEMINI_API_KEY"))
-
-model = genai.GenerativeModel("gemini-1.0-pro")
+API_KEY = os.getenv("GEMINI_API_KEY")
 
 def generate_email_reply(email_text: str, tone: str = "professional"):
 
-    prompt = f"""
-    Write a {tone} email reply to the following message:
+    prompt = f"Write a {tone} reply to this email: {email_text}"
 
-    {email_text}
-    """
+    url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-flash-latest:generateContent"
 
-    response = model.generate_content(prompt)
+    headers = {
+        "Content-Type": "application/json",
+        "X-goog-api-key": API_KEY
+    }
 
-    return response.text
+    data = {
+        "contents":[
+            {
+                "parts":[{"text":prompt}]
+            }
+        ]
+    }
+
+    response = requests.post(url, headers=headers, json=data)
+
+    result = response.json()
+
+    return result["candidates"][0]["content"]["parts"][0]["text"]
